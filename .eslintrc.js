bb41ce3af1e3afd8df13d4f55a7f1da15fd6ab0c
SHEMA Eric <47531860+shemaeric1234@users.noreply.github.com>
2019-09-10 19:14:42 +0200

    bug : addnig untracked changes on lfa feedback [Finish #168403366] (#55)

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,17 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    "class-methods-use-this": 0,
-    "no-console": 0
+    "linebreak-style": "off",
+    "no-console": "off",
+    "camelcase": "off",
+    "import/no-named-as-default": "off",
+    "import/no-named-as-default-member": "off",
+    "consistent-return": "off",
+    "prefer-const": "off",
+    "no-param-reassign": "off",
+    "no-restricted-globals": "off",
+    "no-restricted-syntax": "off",
+    "no-unreachable": "off",
+    "no-useless-catch": "off"
   },
 };
